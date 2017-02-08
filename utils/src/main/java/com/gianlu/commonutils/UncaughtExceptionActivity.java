2b9e7e26947e7aea9b16458d022d2b565bb199c8
Gianlu <altomanigianluca@gmail.com>
2017-02-08 14:57:04 +0100

    UncaughtExceptionActivity

---
+++ utils/src/main/java/com/gianlu/commonutils/UncaughtExceptionActivity.java
@@ -0,0 +1,42 @@
+package com.gianlu.commonutils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+@SuppressWarnings("unused")
+public class UncaughtExceptionActivity extends AppCompatActivity {
+    public static void startActivity(Context context, String appName) {
+        if (context != null)
+            context.startActivity(new Intent(context, UncaughtExceptionActivity.class).putExtra("appName", appName));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unchaught_exception);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.hide();
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        Button email = (Button) findViewById(R.id.uncaughtException_email);
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CommonUtils.sendEmail(UncaughtExceptionActivity.this, getIntent().getStringExtra("appName"));
+            }
+        });
+    }
+}
