c9b77363394e24a54cbc26ec7ce35721e393eed8
Gianlu <altomanigianluca@gmail.com>
2018-05-12 13:13:21 +0200

    Using ActivityWithDialog

--- utils/src/main/java/com/gianlu/commonutils/Analytics/UncaughtExceptionActivity.java
+++ utils/src/main/java/com/gianlu/commonutils/Analytics/UncaughtExceptionActivity.java
@@ -5,14 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.StringRes;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.gianlu.commonutils.CommonUtils;
+import com.gianlu.commonutils.Dialogs.ActivityWithDialog;
 import com.gianlu.commonutils.R;
 
-public class UncaughtExceptionActivity extends AppCompatActivity {
+public class UncaughtExceptionActivity extends ActivityWithDialog {
     public static void startActivity(Context context, @StringRes int appName, Throwable ex) {
         if (context != null && appName != 0) {
             context.startActivity(new Intent(context, UncaughtExceptionActivity.class)
