46d5379bfd39bd921b8f55ec338356e144f38a26
Gianlu <altomanigianluca@gmail.com>
2018-05-17 21:26:16 +0200

    Some annotations

--- utils/src/main/java/com/gianlu/commonutils/Drawer/BaseDrawerProfile.java
+++ utils/src/main/java/com/gianlu/commonutils/Drawer/BaseDrawerProfile.java
@@ -1,11 +1,15 @@
 package com.gianlu.commonutils.Drawer;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 public interface BaseDrawerProfile {
+    @NonNull
     String getProfileName(Context context);
 
+    @NonNull
     String getSecondaryText(Context context);
 
+    @NonNull
     String getInitials(Context context);
 }
