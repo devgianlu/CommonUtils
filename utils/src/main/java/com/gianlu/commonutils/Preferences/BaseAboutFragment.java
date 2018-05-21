29b7f3b4d6200bc94a62ad448c4cb9b543704d7f
Gianlu <altomanigianluca@gmail.com>
2018-05-21 16:22:44 +0200

    New billing mechanism and new icons

--- utils/src/main/java/com/gianlu/commonutils/Preferences/BaseAboutFragment.java
+++ utils/src/main/java/com/gianlu/commonutils/Preferences/BaseAboutFragment.java
@@ -1,117 +1,108 @@
 package com.gianlu.commonutils.Preferences;
 
-import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
-import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.preference.Preference;
+import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.android.vending.billing.IInAppBillingService;
-import com.gianlu.commonutils.Billing.Billing;
-import com.gianlu.commonutils.Billing.Product;
-import com.gianlu.commonutils.Billing.ProductAdapter;
-import com.gianlu.commonutils.Billing.PurchasedProduct;
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.BillingFlowParams;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+import com.android.billingclient.api.SkuDetailsResponseListener;
 import com.gianlu.commonutils.CommonUtils;
 import com.gianlu.commonutils.Dialogs.DialogUtils;
 import com.gianlu.commonutils.Logging;
 import com.gianlu.commonutils.R;
 import com.gianlu.commonutils.Toaster;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
-public abstract class BaseAboutFragment extends AppCompatPreferenceFragment { // TODO: Can be improved, I've written this a long time ago
-    private final static int DONATE_REQUEST_CODE = 75;
-    private String devString;
-    private IInAppBillingService billingService;
-    private ServiceConnection serviceConnection;
+public abstract class BaseAboutFragment extends AppCompatPreferenceFragment implements PurchasesUpdatedListener {
+    private static final List<String> BILLING_PRODUCTS = new ArrayList<>(Arrays.asList(
+            "donation.lemonade",
+            "donation.coffee",
+            "donation.hamburger",
+            "donation.pizza",
+            "donation.sushi",
+            "donation.champagne"
+    ));
+    private final Object billingReady = new Object();
+    private BillingClient billingClient;
+
+    private void handleBillingErrors(@BillingClient.BillingResponse int code) {
+        switch (code) {
+            case BillingClient.BillingResponse.BILLING_UNAVAILABLE:
+            case BillingClient.BillingResponse.SERVICE_UNAVAILABLE:
+            case BillingClient.BillingResponse.SERVICE_DISCONNECTED:
+                Toaster.show(getActivity(), Toaster.Message.FAILED_CONNECTION_BILLING_SERVICE);
+                break;
+            case BillingClient.BillingResponse.USER_CANCELED:
+                Toaster.show(getActivity(), Toaster.Message.BILLING_USER_CANCELLED);
+                break;
+            case BillingClient.BillingResponse.DEVELOPER_ERROR:
+            case BillingClient.BillingResponse.ITEM_UNAVAILABLE:
+            case BillingClient.BillingResponse.FEATURE_NOT_SUPPORTED:
+            case BillingClient.BillingResponse.ITEM_ALREADY_OWNED:
+            case BillingClient.BillingResponse.ITEM_NOT_OWNED:
+            case BillingClient.BillingResponse.ERROR:
+                Toaster.show(getActivity(), Toaster.Message.FAILED_BUYING_ITEM);
+                break;
+            case BillingClient.BillingResponse.OK:
+                break;
+        }
+    }
 
     @Override
+    @CallSuper
     public void onStart() {
         super.onStart();
 
-        if (billingService == null) {
-            serviceConnection = new ServiceConnection() {
-                @Override
-                public void onServiceDisconnected(ComponentName name) {
-                    billingService = null;
-                }
+        billingClient = BillingClient.newBuilder(getActivity()).setListener(this).build();
+        billingClient.startConnection(new BillingClientStateListener() {
+            private boolean retried = false;
 
-                @Override
-                public void onServiceConnected(ComponentName name, IBinder service) {
-                    billingService = IInAppBillingService.Stub.asInterface(service);
-                    if (getCurrentDialog() != null && getCurrentDialog().isShowing()) donate();
-                }
-            };
-
-            getActivity().bindService(new Intent("com.android.vending.billing.InAppBillingService.BIND")
-                    .setPackage("com.android.vending"), serviceConnection, Context.BIND_AUTO_CREATE);
-        }
-    }
-
-    private void unbindBillingService() {
-        Activity activity = getActivity();
-        if (serviceConnection != null && activity != null) {
-            try {
-                activity.unbindService(serviceConnection);
-            } catch (IllegalArgumentException ex) {
-                Logging.log(ex);
-            } finally {
-                serviceConnection = null;
-            }
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        unbindBillingService();
-        super.onDestroy();
-    }
-
-    @Override
-    public void onStop() {
-        unbindBillingService();
-        super.onStop();
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == DONATE_REQUEST_CODE) {
-            if (data.getIntExtra("RESPONSE_CODE", Activity.RESULT_CANCELED) == Activity.RESULT_OK) {
-                try {
-                    PurchasedProduct purchasedProduct = new PurchasedProduct(new JSONObject(data.getStringExtra("INAPP_PURCHASE_DATA")));
-                    if (Objects.equals(purchasedProduct.developerPayload, devString)) {
-                        if (purchasedProduct.purchaseState == PurchasedProduct.PURCHASED) {
-                            Toaster.show(getActivity(), Toaster.Message.THANK_YOU, "Purchased " + purchasedProduct.productId + " with order ID " + purchasedProduct.orderId);
-                        } else if (purchasedProduct.purchaseState == PurchasedProduct.CANCELED) {
-                            Toaster.show(getActivity(), Toaster.Message.PURCHASING_CANCELED);
-                        }
-                    } else {
-                        Toaster.show(getActivity(), Toaster.Message.FAILED_BUYING_ITEM, new Exception("Payloads mismatch!"));
+            @Override
+            public void onBillingSetupFinished(@BillingClient.BillingResponse int code) {
+                if (code == BillingClient.BillingResponse.OK) {
+                    synchronized (billingReady) {
+                        billingReady.notifyAll();
                     }
-                } catch (JSONException ex) {
-                    Toaster.show(getActivity(), Toaster.Message.FAILED_BUYING_ITEM, ex);
+                } else {
+                    handleBillingErrors(code);
                 }
-            } else {
-                Toaster.show(getActivity(), Toaster.Message.PURCHASING_CANCELED);
             }
-        }
+
+            @Override
+            public void onBillingServiceDisconnected() {
+                if (!retried) {
+                    retried = true;
+                    billingClient.startConnection(this);
+                } else {
+                    Toaster.show(getActivity(), Toaster.Message.FAILED_CONNECTION_BILLING_SERVICE);
+                }
+            }
+        });
     }
 
     @StringRes
@@ -130,7 +121,7 @@ public abstract class BaseAboutFragment extends AppCompatPreferenceFragment { //
         findPreference("donateGoogle").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                donate();
+                donate(false);
                 return true;
             }
         });
@@ -189,86 +180,158 @@ public abstract class BaseAboutFragment extends AppCompatPreferenceFragment { //
     @Nullable
     protected abstract Uri getOpenSourceUrl();
 
-    private void donate() {
-        showDialog(DialogUtils.progressDialog(getActivity(), R.string.connectingBillingService));
-        if (billingService == null)
-            return;
+    private void buyProduct(@NonNull SkuDetails product) {
+        BillingFlowParams flowParams = BillingFlowParams.newBuilder()
+                .setSku(product.getSku()).setType(BillingClient.SkuType.INAPP)
+                .build();
 
-        Billing.requestProductsDetails(getActivity(), billingService, new Billing.OnRequestProductDetails() {
+        int responseCode = billingClient.launchBillingFlow(getActivity(), flowParams);
+        if (responseCode != BillingClient.BillingResponse.OK) handleBillingErrors(responseCode);
+    }
+
+    private void showDonateDialog(List<SkuDetails> products) {
+        RecyclerView list = new RecyclerView(getActivity());
+        list.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        list.setAdapter(new SkuAdapter(getActivity(), products, new SkuAdapter.Listener() {
             @Override
-            public void onReceivedDetails(@NonNull final Billing.OnRequestProductDetails handler, final List<Product> products) {
-                final Billing.OnBuyProduct buyHandler = new Billing.OnBuyProduct() {
-                    @Override
-                    public void onGotIntent(@NonNull PendingIntent intent, @NonNull String developerString) {
-                        devString = developerString;
+            public void onItemSelected(@NonNull SkuDetails product) {
+                buyProduct(product);
+                dismissDialog();
+            }
+        }));
 
+        showDialog(new AlertDialog.Builder(getActivity())
+                .setTitle(getString(R.string.donate))
+                .setNegativeButton(android.R.string.cancel, null)
+                .setView(list));
+    }
+
+    private void donate(boolean wasWaiting) {
+        if (!wasWaiting)
+            showDialog(DialogUtils.progressDialog(getActivity(), R.string.connectingBillingService));
+
+        if (billingClient != null && billingClient.isReady()) {
+            SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder();
+            params.setSkusList(BILLING_PRODUCTS).setType(BillingClient.SkuType.INAPP);
+            billingClient.querySkuDetailsAsync(params.build(), new SkuDetailsResponseListener() {
+                @Override
+                public void onSkuDetailsResponse(@BillingClient.BillingResponse int responseCode, List<SkuDetails> products) {
+                    dismissDialog();
+
+                    if (responseCode == BillingClient.BillingResponse.OK)
+                        showDonateDialog(products);
+                    else
+                        handleBillingErrors(responseCode);
+                }
+            });
+        } else {
+            new Thread() {
+                @Override
+                public void run() {
+                    synchronized (billingReady) {
                         try {
-                            getActivity().startIntentSenderForResult(intent.getIntentSender(), DONATE_REQUEST_CODE, new Intent(), 0, 0, 0);
-                        } catch (IntentSender.SendIntentException ex) {
-                            Toaster.show(getActivity(), Toaster.Message.FAILED_CONNECTION_BILLING_SERVICE, ex);
+                            billingReady.wait();
+                            donate(true);
+                        } catch (InterruptedException ex) {
+                            Logging.log(ex);
                         }
                     }
-
-                    @Override
-                    public void onAPIException(int code) {
-                        handler.onAPIException(code);
-                    }
-
-                    @Override
-                    public void onUserCancelled() {
-                        handler.onUserCancelled();
-                    }
-
-                    @Override
-                    public void onFailed(@NonNull Exception ex) {
-                        Toaster.show(getActivity(), Toaster.Message.FAILED_CONNECTION_BILLING_SERVICE, ex);
-                    }
-                };
-                dismissDialog();
-
-                RecyclerView list = new RecyclerView(getActivity());
-                list.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
-                list.setAdapter(new ProductAdapter(getActivity(), products, new ProductAdapter.Listener() {
-                    @Override
-                    public void onItemSelected(@NonNull Product product) {
-                        Billing.buyProduct(getActivity(), billingService, product, buyHandler);
-                    }
-                }));
-
-                showDialog(new AlertDialog.Builder(getActivity())
-                        .setTitle(getString(R.string.donate))
-                        .setNegativeButton(android.R.string.cancel, null)
-                        .setView(list));
-            }
-
-            @Override
-            public void onAPIException(int code) {
-                if (code == Billing.RESULT_BILLING_UNAVAILABLE)
-                    Toaster.show(getActivity(), Toaster.Message.FAILED_CONNECTION_BILLING_SERVICE, "Code: " + code);
-                else
-                    Toaster.show(getActivity(), Toaster.Message.FAILED_BUYING_ITEM, "Code: " + code);
-            }
-
-            @Override
-            public void onUserCancelled() {
-                Toaster.show(getActivity(), Toaster.Message.BILLING_USER_CANCELLED);
-            }
-
-            @Override
-            public void onFailed(@NonNull Exception ex) {
-                Toaster.show(getActivity(), Toaster.Message.FAILED_CONNECTION_BILLING_SERVICE, ex);
-            }
-        });
+                }
+            }.start();
+        }
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        if (billingService != null) {
-            try {
-                getActivity().unbindService(serviceConnection);
-            } catch (IllegalArgumentException ex) {
-                Logging.log(ex);
+    public void onPurchasesUpdated(@BillingClient.BillingResponse int responseCode, List<Purchase> purchases) {
+        if (responseCode == BillingClient.BillingResponse.OK)
+            Toaster.show(getActivity(), Toaster.Message.THANK_YOU, purchases.toString());
+        else
+            handleBillingErrors(responseCode);
+    }
+
+    static class SkuAdapter extends RecyclerView.Adapter<SkuAdapter.ViewHolder> {
+        private final List<SkuDetails> products;
+        private final Listener listener;
+        private final LayoutInflater inflater;
+
+        SkuAdapter(Context context, List<SkuDetails> products, Listener listener) {
+            this.products = products;
+            this.listener = listener;
+            this.inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        @NonNull
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new ViewHolder(parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            final SkuDetails item = products.get(position);
+
+            switch (item.getSku()) {
+                case "donation.lemonade":
+                    holder.icon.setImageResource(R.drawable.ic_juice_64dp);
+                    break;
+                case "donation.coffee":
+                    holder.icon.setImageResource(R.drawable.ic_coffee_64dp);
+                    break;
+                case "donation.hamburger":
+                    holder.icon.setImageResource(R.drawable.ic_hamburger_64dp);
+                    break;
+                case "donation.pizza":
+                    holder.icon.setImageResource(R.drawable.ic_pizza_64dp);
+                    break;
+                case "donation.sushi":
+                    holder.icon.setImageResource(R.drawable.ic_sushi_64dp);
+                    break;
+                case "donation.champagne":
+                    holder.icon.setImageResource(R.drawable.ic_champagne_64dp);
+                    break;
+            }
+
+            holder.title.setText(item.getTitle());
+            holder.description.setText(item.getDescription());
+            holder.buy.setText(item.getPrice());
+            holder.buy.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) listener.onItemSelected(item);
+                }
+            });
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) listener.onItemSelected(item);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return products.size();
+        }
+
+        public interface Listener {
+            void onItemSelected(@NonNull SkuDetails product);
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            final ImageView icon;
+            final TextView title;
+            final TextView description;
+            final Button buy;
+
+            ViewHolder(ViewGroup parent) {
+                super(inflater.inflate(R.layout.product_item, parent, false));
+
+                icon = itemView.findViewById(R.id.productItem_icon);
+                title = itemView.findViewById(R.id.productItem_title);
+                description = itemView.findViewById(R.id.productItem_description);
+                buy = itemView.findViewById(R.id.productItem_buy);
+                buy.setFocusable(false);
             }
         }
     }
