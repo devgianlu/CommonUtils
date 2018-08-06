1b3bbb085731b99ab1d8bc11c8ed6c56bfed9023
Gianlu <altomanigianluca@gmail.com>
2018-08-06 22:35:41 +0200

    Starting implementation

---
+++ utils/src/main/java/com/gianlu/commonutils/Preferences/MaterialAboutVersionItem.java
@@ -0,0 +1,26 @@
+package com.gianlu.commonutils.Preferences;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+
+import com.danielstone.materialaboutlibrary.items.MaterialAboutActionItem;
+import com.gianlu.commonutils.Logging;
+import com.gianlu.commonutils.R;
+
+public class MaterialAboutVersionItem extends MaterialAboutActionItem {
+
+    public MaterialAboutVersionItem(@NonNull Context context) {
+        super(R.string.version, 0, R.drawable.outline_info_24, null);
+
+        String version;
+        try {
+            version = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException ex) {
+            Logging.log(ex);
+            version = context.getString(R.string.unknown);
+        }
+
+        setSubText(version);
+    }
+}
