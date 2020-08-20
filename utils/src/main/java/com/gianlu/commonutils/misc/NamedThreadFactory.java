c86cc0a45d75b7a08bead2e868bc06610b87917f
Gianlu <altomanigianluca@gmail.com>
2020-08-20 16:54:50 +0200

    Added NamedThreadFactory

---
+++ utils/src/main/java/com/gianlu/commonutils/misc/NamedThreadFactory.java
@@ -0,0 +1,26 @@
+package com.gianlu.commonutils.misc;
+
+import androidx.annotation.NonNull;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NamedThreadFactory implements ThreadFactory {
+    private final ThreadGroup group;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private final String namePrefix;
+
+    public NamedThreadFactory(@NonNull String namePrefix) {
+        SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+        this.namePrefix = namePrefix;
+    }
+
+    @Override
+    public Thread newThread(@NonNull Runnable r) {
+        Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+        if (t.isDaemon()) t.setDaemon(false);
+        if (t.getPriority() != Thread.NORM_PRIORITY) t.setPriority(Thread.NORM_PRIORITY);
+        return t;
+    }
+}
