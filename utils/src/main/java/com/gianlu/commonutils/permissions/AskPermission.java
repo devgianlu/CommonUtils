90a3d87817e188b8cc9480067a16225d32e2d8fb
devgianlu <altomanigianluca@gmail.com>
2023-10-06 16:14:46 +0200

    Update AskPermission to use new APIs

--- utils/src/main/java/com/gianlu/commonutils/permissions/AskPermission.java
+++ utils/src/main/java/com/gianlu/commonutils/permissions/AskPermission.java
@@ -5,6 +5,8 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.util.Log;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
@@ -16,11 +18,8 @@ import androidx.fragment.app.FragmentManager;
 import com.gianlu.commonutils.dialogs.DialogUtils;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
-import java.util.concurrent.ThreadLocalRandom;
 
 public final class AskPermission {
-    private static final String PHANTOM_TAG = PhantomFragment.class.getName();
-
     public static void ask(@NonNull final FragmentActivity activity, @NonNull final String permission, @NonNull final Listener listener) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             listener.permissionGranted(permission);
@@ -29,30 +28,24 @@ public final class AskPermission {
 
         if (ContextCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_GRANTED) {
             listener.permissionGranted(permission);
-        } else {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
-                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(activity);
-                listener.askRationale(builder);
-                builder.setPositiveButton(android.R.string.ok, (dialog, which) -> request(activity, permission, listener));
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+            MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(activity);
+            listener.askRationale(builder);
+            builder.setPositiveButton(android.R.string.ok, (dialog, which) -> request(activity, permission, listener));
 
-                DialogUtils.showDialog(activity, builder);
-            } else {
-                request(activity, permission, listener);
-            }
+            DialogUtils.showDialog(activity, builder);
+        } else {
+            request(activity, permission, listener);
         }
     }
 
     private static final String TAG = AskPermission.class.getSimpleName();
 
     private static void request(@NonNull FragmentActivity activity, @NonNull String permission, @NonNull Listener listener) {
-        int code = ThreadLocalRandom.current().nextInt(32);
-        PhantomFragment fragment = PhantomFragment.get(listener, permission, code);
+        PhantomFragment fragment = PhantomFragment.get(listener, permission);
 
         try {
-            activity.getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(fragment, PHANTOM_TAG + code)
-                    .commitNow();
+            activity.getSupportFragmentManager().beginTransaction().add(fragment, null).commitNow();
         } catch (IllegalStateException ex) {
             Log.w(TAG, ex);
         }
@@ -69,14 +62,12 @@ public final class AskPermission {
     public static class PhantomFragment extends Fragment {
         private String permission;
         private Listener listener;
-        private int code;
 
         @NonNull
-        public static PhantomFragment get(Listener listener, String permission, int code) {
+        public static PhantomFragment get(Listener listener, String permission) {
             PhantomFragment fragment = new PhantomFragment();
             fragment.listener = listener;
             fragment.permission = permission;
-            fragment.code = code;
             return fragment;
         }
 
@@ -84,31 +75,27 @@ public final class AskPermission {
         public void onAttach(@NonNull Context context) {
             super.onAttach(context);
 
-            requestPermissions(new String[]{permission}, code);
-        }
-
-        @Override
-        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-            if (listener != null && code == requestCode) {
-                for (int i = 0; i < permissions.length; i++) {
-                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED)
-                        listener.permissionGranted(permissions[i]);
-                    else
-                        listener.permissionDenied(permissions[i]);
+            ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (listener != null) {
+                    if (isGranted) {
+                        listener.permissionGranted(permission);
+                    } else {
+                        listener.permissionDenied(permission);
+                    }
                 }
-            }
 
-            try {
-                if (getActivity() != null) {
-                    FragmentManager manager = getActivity().getSupportFragmentManager();
-                    manager.executePendingTransactions();
-                    manager.beginTransaction()
-                            .remove(this)
-                            .commitAllowingStateLoss();
+                try {
+                    if (getActivity() != null) {
+                        FragmentManager manager = getActivity().getSupportFragmentManager();
+                        manager.executePendingTransactions();
+                        manager.beginTransaction().remove(this).commitAllowingStateLoss();
+                    }
+                } catch (IllegalStateException ex) {
+                    Log.w(TAG, ex);
                 }
-            } catch (IllegalStateException ex) {
-                Log.w(TAG, ex);
-            }
+            });
+
+            requestPermissionLauncher.launch(permission);
         }
     }
 }
